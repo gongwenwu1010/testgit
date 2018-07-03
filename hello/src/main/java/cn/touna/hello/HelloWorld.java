3b30fd2d440b2f66c26b18fe0825376f2e7694d0
gongww <gongwenwuwj66058@touna.cn>
2018-07-03 20:40:34 +0800

    HelloWorld.java更新

--- hello/src/main/java/cn/touna/hello/HelloWorld.java
+++ hello/src/main/java/cn/touna/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package cn.touna.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world01!");
+		System.out.println("Hello world02!");
 	}
 }
